253b2386fef30055bf8f807713bcb2f93b56f308
renzovitalid <renzo.vitali.delgado@gmail.com>
2020-11-19 14:54:23 -0300

    add folder app

---
+++ app/core/erp/tests.py
@@ -0,0 +1,23 @@
+from app.wsgi import *
+
+from core.erp.models import Type
+#listar
+
+# query= Type.objects.all()
+# print (query)
+#
+# #insercion
+# t = Type()
+# t.name ='Pasudasudusa'
+# t.save()
+#
+# # edicion
+
+#  t=Type.objects.get(id=1)
+#  t.name='Accionista VIP'
+#  t.save()
+#
+# #eliminar
+# t=Type.objects.get(id=2)
+# t.delete()
+
